90a37ea3ce8541d457d516e170d20cf7d4f3795c
danglduy <38782010+danglduy@users.noreply.github.com>
2018-09-17 11:12:11 +0000

    reviews index

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,18 @@
 class ReviewsController < ApplicationController
   before_action :find_review, only: :show
 
+  def index
+    @reviews = Review.paginate page: params[:page],
+      per_page: Settings.reviews.per_page
+  end
+
   def show
   end
 
   private
 
   def find_review
-    @review = Review.find_by id: params[:id]
+    film = Film.find_by_id params[:film_id]
+    @review = film.review
   end
 end
